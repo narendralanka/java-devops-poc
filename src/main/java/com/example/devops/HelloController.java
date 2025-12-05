3efd44fd3ab7f910d79dbc8bae2c01e8855ab0d5
NarendraLanka <narendranadh.lanka@gmail.com>
2025-12-05 11:13:53 +0530

    Create HelloController.java

---
+++ src/main/java/com/example/devops/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.devops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from DevOps POC!";
+    }
+}
